a24cb312935e753c6eb3d5a306ca0bcfcce750c1
Kevin Vantus <kevinvantus@gmail.com>
2024-11-23 00:10:11 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rtd-root',
+  imports: [],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'real-time-dashboard';
+}
